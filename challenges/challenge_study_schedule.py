9314cc417b929818d93bc56be3bd727b2076523f
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-25 15:23:26 -0300

    feat: get number of student by target time

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    students_counter = 0
+
+    for start, end in permanence_period:
+        if not isinstance(start, int) or not isinstance(end, int):
+            return None
+
+        if start <= target_time <= end:
+            students_counter += 1
+
+    return students_counter
