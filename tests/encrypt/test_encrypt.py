61f55baf356effb3b6ed0ed70a71be702d8fa940
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-25 15:47:33 -0300

    test: encrypt_message method

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    message = "message"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(message, None)
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(None, 0)
+
+    assert encrypt_message(message, 0) == "egassem"
+
+    assert encrypt_message(message, 1) == "m_egasse"
+
+    assert encrypt_message(message, 2) == "egass_em"
