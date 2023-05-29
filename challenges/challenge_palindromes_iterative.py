d06a8750bce345ea0da5fd5586751080916bbce5
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-29 19:01:56 -0300

    feat: is_palindrome_iterative method

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,15 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+
+    length = len(word)
+
+    for index in range(1, length + 1):
+        first_index = index - 1
+        last_index = length - index
+
+        if word[first_index] != word[last_index]:
+            return False
+
+        if first_index >= last_index:
+            return True
